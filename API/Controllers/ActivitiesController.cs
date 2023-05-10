7ad17d7655e68444bb6ef682d718ae68b0dd17b7
jbalessandro <jb.alessandro@gmail.com>
2023-05-09 22:51:31 -0300

    End of section 6

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -27,8 +27,8 @@ namespace API.Controllers
             return Ok(await Mediator.Send(new Create.Command {Activity = activity}));
         }
 
-        [HttpPut]
-        public async Task<IActionResult> EditActivity(Guid id, Activity activity)
+        [HttpPut("{id}")]
+        public async Task<IActionResult> EditActivity(Guid id, [FromBody]Activity activity)
         {
             activity.Id = id;
             
